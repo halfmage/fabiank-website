2d1f01383311fe13b0a16d06a0d0222c2c88f443
Gerrit Halfmann <gerrit@gerrithalfmann.com>
2019-08-01 23:06:16 +0200

    new styles for landing page

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -1,9 +1,13 @@
 import React from 'react'
-import logo from '../img/logo-fabiank.png'
+import logo from '../img/logo-fabiank-white.png'
+
+import './styles.css'
 
 const IndexPage = ({}) => {
   return (
-    <div style={{
+    <div 
+    className="bg"
+    style={{
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'center',
@@ -16,24 +20,28 @@ const IndexPage = ({}) => {
       }} />
       <h2 style={{
         textAlign: 'center',
-        fontSize: '1.25rem',
+        fontSize: '1.75rem',
         fontWeight: '700',
+        letterSpacing: '-.5px'
       }}>Fabian Klein</h2>
       <h4 style={{
         fontSize: '.825rem',
-        marginBottom: '1rem'
+        marginBottom: '1rem',
+        fontWeight: '300',
       }}>
-        Filmmaker. Photographer
+        making films. taking photos
       </h4>
-      <div style={{fontSize:'.75rem'}}>+49 (0) 1762 1490 478</div>
-      <div style={{fontSize:'.75rem'}}>mail@fabiank.de</div>
+      <div style={{fontSize:'.75rem'}}>t. +49 (0) 1762 1490 478</div>
+      <div style={{fontSize:'.75rem', marginBottom:'1rem'}}>e. mail@fabiank.de</div>
+
+      <div style={{fontSize:'.75rem'}}>Remscheider Straße 19</div>
+      <div style={{fontSize:'.75rem'}}>D-42659 Solingen</div>
       <div style={{
         display: 'flex',
         marginTop: '1rem'
       }}>
         <a style={{fontSize:'14px',margin:'0 .5rem'}} target="_blank" href="https://vimeo.com/fabianklein">Vimeo</a>
         <a style={{fontSize:'14px',margin:'0 .5rem'}} target="_blank" href="https://www.instagram.com/fabianklein_/">Instagram</a>
-        <a style={{fontSize:'14px',margin:'0 .5rem'}} target="_blank" href="https://www.facebook.com/Fabian-Klein-320664735407574/">Facebook</a>
       </div>
     </div>
   )
